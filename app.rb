0e04e6eb5b1df24a465970c52ee7c460ea1bdecb
Nikita <nekytnp@gmail.com>
2022-01-18 21:54:29 +0200

    Time formatter added

--- app.rb
+++ app.rb
@@ -1,7 +1,32 @@
+require_relative "time_formatter"
+
 class App
 
-  def call
+  HEADERS = { "Content-Type" => "text/plain" }
 
+  def call(env)
+    if env["PATH_INFO"] == '/time'
+      prepare_response(env["QUERY_STRING"])
+    else
+      response(404, "Not found")
+    end
+  end
+
+  private
+
+  def prepare_response(query_string)
+    time_formatter = TimeFormatter.new
+    time_formatter.call(query_string)
+
+    if time_formatter.success?
+      response(200, time_formatter.time_string)
+    else
+      response(400, time_formatter.invalid_string)
+    end
+  end
+
+  def response(code, body)
+    [code, HEADERS, ["#{body}\n"]]
   end
   
 end
---
+++ time_formatter.rb
@@ -0,0 +1,31 @@
+class TimeFormatter
+  # attr_reader :unknown_params, :time_params
+
+  TIME_FORMAT = {
+    'year' => '%Y',
+    'month' => '%m',
+    'day' => '%d',
+    'hour' => '%H',
+    'minute' => '%M',
+    'second' => '%S'
+  }.freeze
+
+  def call(query_string)
+    format_params = URI.decode_www_form(query_string).to_h['format'] || ''
+    @time_params = format_params.split(',').map(&:strip).map(&:downcase).uniq
+    @unknown_params = @time_params - TIME_FORMAT.keys
+    # @unknown_params = time_params - TIME_FORMAT.keys
+  end
+
+  def time_string
+    Time.now.strftime(@time_params.map { |param| TIME_FORMAT[param] }.compact.join('-'))
+  end
+
+  def invalid_string
+    @time_params.any? ? "Unknown time format #{@unknown_params}!" : "Not found format params!"
+  end
+
+  def success?
+    @time_params.any? && @unknown_params.empty?
+  end
+end
